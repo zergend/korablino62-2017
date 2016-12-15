3caf75a2075d0426ca03e66cf5b0a87a406d04b1
zergend <zergend@gmail.com>
2016-12-15 16:53:42 +0300

    15 12 2016 вечер

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,13 @@
-module.exports = funcrion(grunt) {
+module.exports = function(grunt) {
+  grunt.loadNpmTasks("grunt-contrib-less");
 
-  grunt.initConfig({
-
-  });
-
+    grunt.initConfig({
+      less: {
+        style: {
+          files: {
+            "css/style1.css": "less/style.less"
+          }
+        }
+      }
+    });
 };
